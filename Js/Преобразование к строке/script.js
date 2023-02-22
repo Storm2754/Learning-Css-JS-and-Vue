ff0c4e5ba1bab8fd95471b8ebf2720d49f2c56e2
Gubsky_Maxim -_- <96713818+Storm2754@users.noreply.github.com>
2023-02-23 00:06:49 +0300

    commit

---
+++ Js/Преобразование к строке/script.js
@@ -0,0 +1,7 @@
+let f = String(123);
+alert (f);
+
+let a,s;
+a=23;
+s=9;
+alert (String(a)+String(s));
---
+++ Js/Преобразование к числу/script.js
@@ -0,0 +1,8 @@
+let a = parseInt('12px');
+alert (a); //выводит 12
+
+let s =parseInt('45,3px');
+alert (s); //выводит 45
+
+let d = parseFloat('22.4px');
+alert (d); //выводит 22,4
